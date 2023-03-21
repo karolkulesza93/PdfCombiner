58badbb7e37f06455ac3f65adcb6505024c433fc
Karol <kar.kul1993@gmail.com>
2023-03-21 20:13:34 +0100

    Add project files.

---
+++ PdfCombiner/PdfCombinerWindow.cs
@@ -0,0 +1,10 @@
+namespace PdfCombiner
+{
+    public partial class PdfCombinerWindow : Form
+    {
+        public PdfCombinerWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
